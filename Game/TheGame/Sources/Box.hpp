18521fad306e0c15d476b9239b751d455a42a035
makdenis <makdenis.1997@gmail.com>
2018-05-22 23:41:13 +0300

    finish added

--- Game/TheGame/Sources/Box.hpp
+++ Game/TheGame/Sources/Box.hpp
@@ -11,15 +11,21 @@ class Box : public Actor
 	GENERATED_BODY(Box, Actor);
 
 public:
+	
 	Box()
 	{
+		const FVector2 wallSize(3,2);
 		body = CreateSubcomponent<BoxColision>("Body");
+		boxSprite = CreateSubcomponent<SpriteComponent>("WallSprite", "finish.png");
+		boxSprite->SetExtents(wallSize);
+		boxSprite->AttachTo(body);
 		body->SetExtents(FVector(3, 2, 0.6f));
 		body->SetMass(0);
 	}
 
 protected:
 	BoxColision* body;
+	SpriteComponent* boxSprite;
 };
 
 #endif // !BOX_HPP
